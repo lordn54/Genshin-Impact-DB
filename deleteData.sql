fd909f2a25de793455b53aea815b37908b01eca0
lordn54 <56842936+lordn54@users.noreply.github.com>
2020-11-16 17:14:39 -0700

    Create deleteData.sql

---
+++ deleteData.sql
@@ -0,0 +1,9 @@
+DELETE FROM Characters;
+DELETE FROM Character_Rewards;
+DELETE FROM Domain_Rewards;
+DELETE FROM Domains;
+DELETE FROM Elemental_Interactions;
+DELETE FROM Elite_Rewards;
+DELETE FROM Elites;
+DELETE FROM Enemy_Rewards;
+DELETE FROM Enemies;
